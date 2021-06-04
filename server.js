e1fe064d2f41d0d5ab898f8cee919c4f3136e4f4
Sebastian Montes <sebastian.montes@moove-it.com>
2021-06-04 16:38:51 -0500

    Fix deploy heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-if (process.env === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
